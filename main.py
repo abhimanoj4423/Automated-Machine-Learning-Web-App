2429897b5bc54d6e0475dcbe4b40c9b7b33ae0e7
Abhijith Manoj <123548712+abhimanoj4423@users.noreply.github.com>
2024-05-08 21:29:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ if choice == "Model Building":
             compare_df = cl.pull()
             cl.save_model(best_model, 'best_model')
             st.dataframe(compare_df)
+            st.text_area("    ", value=best_model)
 
     if choice1 == 'Clustering':
         empty = []
